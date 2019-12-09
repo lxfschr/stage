7c7fcfd41d007e30c73615b499f9b50bf33d1568
Alex Fischer <lxfschr@gmail.com>
2019-12-08 21:52:34 -0800

    cleaned up code

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,114 +1,60 @@
 import textureShaderVert from 'shaders/texture.vs.glsl';
 import textureShaderFrag from 'shaders/texture.fs.glsl';
-
-class Matrix4 extends Float32Array {
-    static identity() {
-        const mat4 = new Float32Array(16);
-        for (let i = 0; i < mat4.length; i+=5) {
-            mat4[i] = 1; 
-        }
-        return mat4;
-    }
-}
+import Matrix4 from "components/Matrix4";
+import MathUtils from "components/MathUtils";
+import ShaderUtil from "components/ShaderUtil";
+import GLInstance from "components/GLInstance";
 
 async function Application() {
-    const canvas = document.getElementById('surface');
-    const gl = canvas.getContext('webgl');
+    let width = 600, height = 480;
+    // width = window.innerWidth, height = window.innerHeight;
+    const vShaderSource = textureShaderVert, fShaderSource = textureShaderFrag;
+    const gl = GLInstance('surface').fSetSize(width, height).fClear();
 
-    if (!gl) {
-        console.log('WebGL not supportedm, falling back on experimental-webgl');
-        gl - canvas.getContext('experimental-webgl');
-    }
-
-    if (!gl) {
-        console.log('Your browser does not support WebGL');
-    }
-
-    canvas.width = 800;//window.innerWidth;
-    canvas.height = 600;//window.innerHeight;
-    gl.viewport(0, 0, canvas.width, canvas.height);
-
-    gl.clearColor(0.75, 0.85, 0.8, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT);
-
-    //
-    // Create shaders
-    //
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-    gl.shaderSource(vertexShader, textureShaderVert);
-    gl.shaderSource(fragmentShader, textureShaderFrag);
-
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        console.error('ERROR compling vertex shader!', gl.getShaderInfoLog(vertexShader));
-    }
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        console.error('ERROR compling fragment shader!', gl.getShaderInfoLog(fragmentShader));
-    }
-
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
-        return;
-    }
-    gl.validateProgram(program);
-    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
-        return;
-    }
+    const program = ShaderUtil.shaderProgram(gl, vShaderSource, fShaderSource);
 
     //
     // Create buffer
     //
     const triangleVertices =
     [
-        // X, Y      U, V
-        0.0, 0.5,    0.4, 0.5,
-        -0.5, -0.5,  0.8, 0.9,
-        0.5, -0.5,   0.0, 1.0,
+        // X, Y, Z        U, V
+        0.0, 0.5, 0.0,    0.4, 0.5,
+        -0.5, -0.5, 0.0,  0.8, 0.9,
+        0.5, -0.5, 0.0,   0.0, 1.0,
     ];
 
     const triangleVertexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
 
-    const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-    const texCoordAttribLocation = gl.getAttribLocation(program, 'verTexCoord');
-    gl.vertexAttribPointer(
-        positionAttribLocation, // Attribute location
-        2, // Number of elements per attribute
+    ShaderUtil.enableVertexAttribArray(gl, program, 'vertPosition', 
+        3, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE,
-        4 * Float32Array.BYTES_PER_ELEMENT,// Size of individual vertex
-        0 // Offset from the beginning of a single vertex to this attribute
+        5 * Float32Array.BYTES_PER_ELEMENT,// Size of individual vertex
+        0 // Offset from the beginning of a single vertex to this attribute)
     );
-    gl.vertexAttribPointer(
-        texCoordAttribLocation, // Attribute location
+    
+    ShaderUtil.enableVertexAttribArray(gl, program, 'verTexCoord', 
         2, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE,
-        4 * Float32Array.BYTES_PER_ELEMENT,// Size of individual vertex
-        2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+        5 * Float32Array.BYTES_PER_ELEMENT,// Size of individual vertex
+        3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
     );
 
-    gl.enableVertexAttribArray(positionAttribLocation);
-    gl.enableVertexAttribArray(texCoordAttribLocation);
 
     //
     // Create Texture
     //
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    const texture = ShaderUtil.createTexture(gl, 
+        gl.TEXTURE_2D, // target e.g. TEXTURE_2D, TEXTURE_CUBE_MAP, TEXTURE_3D (WebGL2 only)
+        gl.CLAMP_TO_EDGE, // Wrapping function for s e.g. CLAMP_TO_EDGE, REPEAT, MIRRORED_REPEAT 
+        gl.CLAMP_TO_EDGE, // Wrapping function for t e.g. CLAMP_TO_EDGE, REPEAT, MIRRORED_REPEAT 
+        gl.LINEAR, // Texture minification filter e.g. LINEAR, NEAREST, NEAREST_MIPMAP_NEAREST
+        gl.LINEAR // Texture magnification filter LINEAR or NEAREST
+    );
 
     const imageElement = document.getElementById('texture-image');
 
@@ -116,29 +62,46 @@ async function Application() {
         gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE,
         imageElement);
 
-    // gl.bindTexture(gl.TEXTURE_2D, null);
-
     //
     // Main render loop
     //
     gl.useProgram(program);
 
-    var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
-	var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
-	var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+    const matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+	const matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+	const matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
 
-	var worldMatrix = Matrix4.identity();
-	var viewMatrix = Matrix4.identity();
-    var projMatrix = Matrix4.identity();
+    let worldMatrix = Matrix4.identity();
+	let viewMatrix = new Matrix4();
+    let projMatrix = new Matrix4();
 
+    const target = [0, 0, 0];
+    const cameraPosition = [0, 0, -5];
+    const cameraUp = [0, 1, 0];
+    Matrix4.lookAt(viewMatrix, cameraPosition, target, cameraUp);
 
+    Matrix4.perspective(projMatrix, MathUtils.toRadians(45), width / height, 0.1, 1000.0);
 
     gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
-	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
     
+    const identityMatrix = Matrix4.identity();
+    let angle = 0;
+    const loop = () => {
+        angle = performance.now() / 1000 / 6 * 2 * Math.PI;
+        Matrix4.rotate(worldMatrix, identityMatrix, angle, [0, 1, 0]);
+        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.clearColor(0.75, 0.85, 0.8, 1.0);
+        gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+        requestAnimationFrame(loop);
+    };
+    
+    requestAnimationFrame(loop);
 }
 
 export default Application;
